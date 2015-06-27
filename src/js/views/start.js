4eca1431e5ea8ab27c4034447ab1e6c83c6a0dae
Frank Wilson <frank@zenlambda.com>
2015-06-27 14:14:17 +0100

    Hard wrap 'start' view jsx

--- src/js/views/start.js
+++ src/js/views/start.js
@@ -53,57 +53,82 @@ module.exports = React.createClass({
     render: function () {
         return (
             <UI.View>
-                <UI.Headerbar type="default" label="TouchstoneJS" />
+                <UI.Headerbar type="default" label="React Europe 2015" />
                 <UI.ViewContent grow scrollable>
                     <div className="panel-header text-caps">Bars</div>
                     <div className="panel">
-                        <Link component="div" to="component-headerbar" viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="component-headerbar"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">Header Bar</div>
                         </Link>
-                        <Link component="div" to="component-headerbar-search" viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="component-headerbar-search"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">Header Bar Search</div>
                         </Link>
-                        <Link component="div" to="component-alertbar" viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="component-alertbar"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">Alert Bar</div>
                         </Link>
-                        <Link component="div" to="component-footerbar" viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="component-footerbar"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">Footer Bar</div>
                         </Link>
                     </div>
                     <div className="panel-header text-caps">Lists</div>
                     <div className="panel">
-                        <Link component="div" to="component-simple-list" viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="component-simple-list"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">Simple List</div>
                         </Link>
-                        <Link component="div" to="component-complex-list" viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="component-complex-list"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">Complex List</div>
                         </Link>
                         {/* This is covered in other components
-                        <Link component="div" to="component-categorised-list" viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="component-categorised-list"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">Categorised List</div>
                         </Link>*/}
                     </div>
                     <div className="panel-header text-caps">UI Elements</div>
                     <div className="panel">
-                        <Link component="div" to="component-toggle"   viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="component-toggle"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">Toggle</div>
                         </Link>
-                        <Link component="div" to="component-form"     viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="component-form"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">Form Fields</div>
                         </Link>
-                        <Link component="div" to="component-passcode" viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="component-passcode"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">Passcode / Keypad</div>
                         </Link>
-                        <Tappable component="div" onTap={this.showLoadingPopup} className="list-item is-tappable">
+                        <Tappable component="div" onTap={this.showLoadingPopup}
+                            className="list-item is-tappable">
                             <div className="item-inner">Loading Spinner</div>
                         </Tappable>
                     </div>
                     <div className="panel-header text-caps">Application State</div>
                     <div className="panel">
-                        <Link component="div" to="transitions" viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="transitions"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">View Transitions</div>
                         </Link>
-                        <Link component="div" to="component-feedback" viewTransition="show-from-right" className="list-item is-tappable">
+                        <Link component="div" to="component-feedback"
+                            viewTransition="show-from-right"
+                            className="list-item is-tappable">
                             <div className="item-inner">View Feedback</div>
                         </Link>
                     </div>
