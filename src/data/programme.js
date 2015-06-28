b133b383da70570367b52b73dfb1e0096208131f
Frank Wilson <frank@zenlambda.com>
2015-06-28 14:40:07 +0100

    Rapidly transcribed speaker data, mistakes likely

---
+++ src/data/programme.js
@@ -0,0 +1,228 @@
+module.exports = [
+    {
+        title: "Keynote",
+        speakers: [{
+            firstName: "Christopher",
+            lastName: "Chedeau",
+            twitter: "https://twitter.com/Vjeux",
+            web: "http://blog.vjeux.com/",
+            github: "https://github.com/vjeux",
+            pic: "https://www.react-europe.org/images/christopher-chedeau.jpg",
+        }]
+    },
+    {
+        title: "Inline Styles: themes, media queries, contexts, and when it's best to use CSS",
+        speakers: [{
+            firstName: "Michael",
+            lastName: "Chan",
+            twitter: "https://twitter.com/chantastic",
+            github: "https://github.com/chantastic",
+            web: "http://chantastic.io/",
+            pic: "https://www.react-europe.org/images/michael-chan.jpg",
+        }]
+    },
+    {
+        title: "Flux over the Wire",
+        speakers: [{
+            firstName: "Elie",
+            lastName: "Rotenberg",
+            twitter: "https://twitter.com/elierotenberg",
+            github: "https://github.com/elierotenberg",
+            web: "https://elie.rotenberg.io/",
+            pic: "https://www.react-europe.org/images/elie-rotenberg.jpg",
+        }]
+    },
+    {
+        title: "React Native: Building Fluid User Experiences",
+        speakers: [{
+            firstName: "Spencer",
+            lastName: "Ahrens",
+            github: "https://github.com/sahrens",
+            facebook: "https://www.facebook.com/spencer",
+            linkedin: "https://www.linkedin.com/pub/spencer-ahrens/6/733/360",
+        }]
+    },
+    {
+        title: "Exploring GraphQL",
+        speakers: [{
+            firstName: "Lee",
+            lastName: "Byron",
+            twitter: "https://twitter.com/leeb",
+            linkedin: "http://leebyron.com/",
+            github: "https://github.com/leebyron",
+            pic: "https://www.react-europe.org/images/lee-byron.jpg",
+        }]
+    },
+    {
+        title: "Don't Rewrite, React!",
+        speakers: [{
+            firstName: "Ryan",
+            lastName: "Florence",
+            twitter: "https://twitter.com/ryanflorence",
+            web: "https://reactjs-training.com/",
+            github: "https://github.com/ryanflorence",
+            pic: "https://www.react-europe.org/images/ryan-florence.jpg",
+        }]
+    },
+    {
+        title: "Live React: Hot Reloading with Time Travel",
+        speakers: [{
+            firstName: "Dan",
+            lastName: "Abramov",
+            twitter: "https://twitter.com/dan_abramov",
+            web: "https://medium.com/@dan_abramov",
+            github: "https://github.com/gaearon",
+            pic: "https://www.react-europe.org/images/dan-abramov.jpg"
+        }]
+    },
+    {
+        title: "Relay: An Application Framework For React",
+        speakers: [{
+            firstName: "Joseph",
+            lastName: "Savona",
+            twitter: "https://twitter.com/en_js",
+            web: "http://josephsavona.com/",
+            github: "https://github.com/josephsavona",
+            pic: "https://www.react-europe.org/images/joseph-savona.jpg",
+        }]
+    },
+    {
+        title: "Back to Text UI",
+        speakers: [{
+            firstName: "Mikhail",
+            lastName: "Davydov",
+            twitter: "https://twitter.com/azproduction",
+            linkedin: "http://azproduction.ru/",
+            github: "https://github.com/azproduction",
+            pic: "https://www.react-europe.org/images/mikhail-davydov.jpg",
+        }]
+    },
+    {
+        title: "DOM as a Second-class Citizen",
+        speakers: [{
+            firstName: "Sebastian",
+            lastName: "Markbåge",
+            twitter: "https://twitter.com/sebmarkbage",
+            linkedin: "https://www.linkedin.com/in/sebmarkbage",
+            github: "https://github.com/sebmarkbage/",
+            pic: "https://www.react-europe.org/images/SebastianProfile.jpg",
+        }]
+    },
+    {
+        title: "Lightning Talks"
+    },
+    {
+        title: "Improving Your Workflow With Code Transformation",
+        speakers: [{
+            firstName: "Sebastian",
+            lastName: "McKenzie",
+            twitter: "https://twitter.com/sebmck",
+            web: "https://babeljs.io/",
+            github: "https://github.com/sebmck",
+            pic: "https://www.react-europe.org/images/sebastian-mcKenzie.png"
+        }]
+    },
+    {
+        title: "The State of Animation in React",
+        speakers: [{
+            firstName: "Cheng",
+            lastName: "Lou",
+            twitter: "https://twitter.com/_chenglou",
+            // LinkedIn URL listed on the conference page doesn't work for me :-(
+            pic: "https://www.react-europe.org/images/cheng-lou.jpg"
+        }]
+    },
+    {
+        title: "Simplifying the data layer",
+        speakers: [{
+            firstName: "Kevin",
+            lastName: "Robinson",
+            twitter: "https://twitter.com/krob",
+            linkedin: "https://www.linkedin.com/pub/kevin-robinson/4/676/11a",
+            github: "https://github.com/kevinrobinson",
+            pic: "https://www.react-europe.org/images/kevin-robinson.png",
+        }]
+    },
+    {
+        title: "Going Mobile with React",
+        speakers: [{
+            firstName: "Jed",
+            lastName: "Watson",
+            twitter: "https://twitter.com/JedWatson",
+            web: "http://thinkmill.com.au/",
+            github: "https://github.com/JedWatson",
+            pic: "https://www.react-europe.org/images/jed-watson.jpg",
+        }]
+    },
+    {
+        title: "React Router",
+        speakers: [{
+            firstName: "Michael",
+            lastName: "Jackson",
+            twitter: "https://twitter.com/mjackson",
+            web: "https://reactjs-training.com/",
+            github: "https://github.com/mjackson",
+            pic: "https://www.react-europe.org/images/michael-jackson.jpg",
+        }]
+    },
+    {
+        title: "Creating a GraphQL Server",
+        speakers: [{
+            firstName: "Nick",
+            lastName: "Schrock",
+            twitter: "https://twitter.com/schrockn",
+            facebook: "http://www.facebook.com/schrockn",
+            github: "https://github.com/schrockn",
+            pic: "https://www.react-europe.org/images/schrockn.jpg",
+        },
+        {
+            firstName: "Dan",
+            lastName: "Schafer",
+            twitter: "https://twitter.com/dlschafer",
+            facebook: "https://www.facebook.com/dschafer",
+            github: "https://github.com/dschafer",
+            pic: "https://www.react-europe.org/images/dlschafer.jpg"
+        }]
+    },
+    {
+        title: "Isomorphic Flux",
+        speakers: [{
+            firstName: "Michael",
+            lastName: "Ridgway",
+            twitter: "https://twitter.com/TheRidgway",
+            web: "http://theridgway.com/",
+            github: "https://github.com/mridgway",
+            pic: "https://www.react-europe.org/images/michael-ridgway.jpg"
+        }]
+    },
+    {
+        title: "Building submarines that don't leak",
+        speakers: [{
+            firstName: "Aria",
+            lastName: "Buckles",
+            twitter: "https://twitter.com/ariabuckles",
+            web: "https://sourcegraph.com/ariabuckles",
+            github: "https://github.com/ariabuckles",
+            pic: "https://www.react-europe.org/images/aria-buckles.jpg",
+        }]
+    },
+    {
+        title: "How React & Flux Turn Applications Into Extensible Platforms",
+        speakers: [{
+            firstName: "Evan Morikawa ",
+            lastName: "Evan Morikawa ",
+            twitter: "https://twitter.com/E0M",
+            linkedin: "https://www.linkedin.com/in/evanmorikawa",
+            github: "https://github.com/emorikawa",
+            pic: "https://www.react-europe.org/images/evan-morikawa.jpg"
+        },
+        {
+            firstName: "Ben",
+            lastName: "Gotow",
+            twitter: "https://twitter.com/bengotow",
+            linkedin: "http://foundry376.com/",
+            github: "https://github.com/bengotow",
+            pic: "https://www.react-europe.org/images/ben-gotow.jpg"
+        }]
+    }
+]
