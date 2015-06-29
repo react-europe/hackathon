acd3bb5b2d90ddfafaed9b01fbcbff004a5459ce
Frank Wilson <frank@zenlambda.com>
2015-06-29 18:43:33 +0100

    j'adoube

--- src/js/views/component/list-complex.js
+++ src/js/views/component/list-complex.js
@@ -11,35 +11,46 @@ var ComplexListItem = React.createClass({
 	mixins: [Navigation],
 
 	render: function () {
-		
+
 		var initials = this.props.user.name.first.charAt(0).toUpperCase() +
 			this.props.user.name.last.charAt(0).toUpperCase();
 
 		return (
-			<Link to="details" viewTransition="show-from-right" params={{ user: this.props.user, prevView: 'component-complex-list' }} className="list-item" component="div">
-				<UI.ItemMedia avatar={this.props.user.img} avatarInitials={initials} />
+			<Link to="details" viewTransition="show-from-right"
+				params={{
+					user: this.props.user,
+					prevView: 'component-complex-list',
+				}} className="list-item" component="div">
+				<UI.ItemMedia avatar={this.props.user.img}
+					avatarInitials={initials} />
 				<div className="item-inner">
 					<div className="item-content">
-						<div className="item-title">{[this.props.user.name.first, this.props.user.name.last].join(' ')}</div>
-						<div className="item-subtitle">{this.props.user.location}</div>
+						<div className="item-title">{[
+							this.props.user.name.first,
+							this.props.user.name.last].join(' ')}</div>
+						<div className="item-subtitle">
+							{this.props.user.location}
+						</div>
 					</div>
-					<UI.ItemNote type="default" label={this.props.user.joinedDate.slice(-4)} icon="ion-chevron-right" />
+					<UI.ItemNote type="default"
+						label={this.props.user.joinedDate.slice(-4)}
+						icon="ion-chevron-right" />
 				</div>
 			</Link>
 		);
-	}
+	},
 });
 
 var ComplexList = React.createClass({
 	render: function () {
 
 		var users = [];
-		
+
 		this.props.users.forEach(function (user, i) {
 			user.key = 'user-' + i;
 			users.push(React.createElement(ComplexListItem, { user: user }));
 		});
-		
+
 		return (
 			<div>
 				<div className="panel panel--first avatar-list">
@@ -47,7 +58,7 @@ var ComplexList = React.createClass({
 				</div>
 			</div>
 		);
-	}
+	},
 });
 
 module.exports = React.createClass({
@@ -58,12 +69,14 @@ module.exports = React.createClass({
 		return (
 			<UI.View>
 				<UI.Headerbar type="default" label="Complex List">
-					<UI.HeaderbarButton showView="home" viewTransition="reveal-from-right" label="Back" icon="ion-chevron-left" />
+					<UI.HeaderbarButton showView="home"
+						viewTransition="reveal-from-right" label="Back"
+						icon="ion-chevron-left" />
 				</UI.Headerbar>
 				<UI.ViewContent grow scrollable>
 					<ComplexList users={People} />
 				</UI.ViewContent>
 			</UI.View>
 		);
-	}
+	},
 });
